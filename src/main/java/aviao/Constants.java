b42b4bbac6a356fb20aca86df32c4ebc504815c1
23Daniel3 <daneilcipriani321@gmail.com>
2025-03-27 13:36:31 -0300

    build code

--- src/main/java/aviao/Constants.java
+++ src/main/java/aviao/Constants.java
@@ -2,9 +2,8 @@ package aviao;
 
 public class Constants {
 
-    public static class ControllerConstants {
-        public static final double[] X_VALUES = {0.0, 0.2, 0.5, 0.7, 0.9, 1.0}; // Entrada (0 a 1)
-        public static final double[] Y_VALUES = {0.0, 0.2, 0.5, 0.7, 0.9, 1.0}; // Saída desejada    
-   
-    }
+  public static class ControllerConstants {
+    public static final double[] X_VALUES = {0.0, 0.2, 0.5, 0.7, 0.9, 1.0}; // Entrada (0 a 1)
+    public static final double[] Y_VALUES = {0.0, 0.2, 0.5, 0.7, 0.9, 1.0}; // Saída desejada
+  }
 }
--- src/main/java/aviao/util/ThrottleMap.java
+++ src/main/java/aviao/util/ThrottleMap.java
@@ -1,41 +1,40 @@
 package aviao.util;
 
+import java.util.function.Supplier;
 import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 
-import java.util.function.Supplier;
-
 public class ThrottleMap {
 
-    private final double[] xValuesSupplier;
-    private final double[] yValuesSupplier;
-    private PolynomialSplineFunction throttleCurve;
+  private final double[] xValuesSupplier;
+  private final double[] yValuesSupplier;
+  private PolynomialSplineFunction throttleCurve;
 
-    public ThrottleMap(double[] xValues, double[] yValues) {
-        this.xValuesSupplier = xValues;
-        this.yValuesSupplier = yValues;
-        updateThrottleCurve();
+  public ThrottleMap(double[] xValues, double[] yValues) {
+    this.xValuesSupplier = xValues;
+    this.yValuesSupplier = yValues;
+    updateThrottleCurve();
+  }
+
+  private void updateThrottleCurve() {
+    double[] xValues = xValuesSupplier;
+    double[] yValues = yValuesSupplier;
+    PolynomialFunction[] polynomials = new PolynomialFunction[xValues.length - 1];
+
+    for (int i = 0; i < polynomials.length; i++) {
+      double a = yValues[i];
+      double b = (yValues[i + 1] - yValues[i]) / (xValues[i + 1] - xValues[i]);
+      polynomials[i] = new PolynomialFunction(new double[] {a, b});
     }
+    throttleCurve = new PolynomialSplineFunction(xValues, polynomials);
+  }
 
-    private void updateThrottleCurve() {
-        double[] xValues = xValuesSupplier;
-        double[] yValues = yValuesSupplier;
-        PolynomialFunction[] polynomials = new PolynomialFunction[xValues.length - 1];
+  public double applyThrottle(Supplier<Double> input) {
+    return throttleCurve.value(Math.min(1.0, Math.max(0.0, input.get())));
+  }
 
-        for (int i = 0; i < polynomials.length; i++) {
-            double a = yValues[i];
-            double b = (yValues[i + 1] - yValues[i]) / (xValues[i + 1] - xValues[i]);
-            polynomials[i] = new PolynomialFunction(new double[]{a, b});
-        }
-        throttleCurve = new PolynomialSplineFunction(xValues, polynomials);
-    }
-
-    public double applyThrottle(Supplier<Double> input) {
-        return throttleCurve.value(Math.min(1.0, Math.max(0.0, input.get())));
-    }
-
-    public double applyThrottleAbs(Supplier<Double> input) {
-        double magnitude = Math.min(1.0, Math.max(0.0, Math.abs(input.get())));
-        return Math.copySign(throttleCurve.value(magnitude), input.get());
-    }
+  public double applyThrottleAbs(Supplier<Double> input) {
+    double magnitude = Math.min(1.0, Math.max(0.0, Math.abs(input.get())));
+    return Math.copySign(throttleCurve.value(magnitude), input.get());
+  }
 }
