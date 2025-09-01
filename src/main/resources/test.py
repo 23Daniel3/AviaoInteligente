47d608b1350c5bde0c197d41ce8c3a8da6b5ecb1
23Daniel3 <danielcipriani321@gmail.com>
2025-09-01 14:31:53 -0300

    Organizado códico esp

--- src/main/resources/test.py
+++
@@ -1,7 +0,0 @@
-import serial
-ser = serial.Serial("COM4", 115200, timeout=1)
-
-while True:
-    line = ser.readline().decode(errors="ignore").strip()
-    if line:
-        print("Recebido cru:", repr(line))
