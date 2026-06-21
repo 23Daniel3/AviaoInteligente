c046a33667c6030c7ccaec439cc29d6f3c0ab6f4
Daniel Cipriani <danielcipriani321@gmail.com>
2026-06-20 22:47:12 -0300

    cleaned code

--- src/main/cpp/lib/SubsystemBase.h
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-
-class SubsystemBase {
-public:
-    virtual ~SubsystemBase() {} // Destrutor virtual é boa prática em C++
-    
-    // Método que será chamado em todo ciclo do loop (igual no Java)
-    virtual void periodic() {} 
-};
